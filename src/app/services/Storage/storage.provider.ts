d67a3b94ab804ea82b5910c6dcb4129e969f3ee7
JWileczek <john.wileczek@prominentedge.com>
2019-06-26 20:18:00 -0400

    Modifying readme

--- src/app/services/Storage/storage.provider.ts
+++ src/app/services/Storage/storage.provider.ts
@@ -1,20 +1,20 @@
 import {StorageService} from './storage.service';
 import {AppConfigService} from '../../app.config';
-import {FilesystemStrategy} from './Strategies/file.strategy';
-import {S3Strategy} from './Strategies/s3.strategy';
+import {FilesystemStrategy} from './Backends/file.backend';
+import {S3Backend} from './Backends/s3.backend';
 
 export enum StorageBackends {
   S3 = 's3',
   FS = 'fs',
 }
 
-export const storageFactory = () => {
+const storageFactory = () => {
   const settings = AppConfigService.settings;
   const backendConfig = settings.backendConfigs[settings.defaultBackend];
   let backend = null;
   switch (settings.defaultBackend) {
     case StorageBackends.S3: {
-      backend = new S3Strategy(backendConfig);
+      backend = new S3Backend(backendConfig);
       break;
     }
     case StorageBackends.FS: {
@@ -25,8 +25,7 @@ export const storageFactory = () => {
   return backend;
 };
 
-export let storageDefaultProvider =
-  {
+export let storageDefaultProvider = {
     provide: StorageService,
     useFactory: storageFactory,
     deps: []
