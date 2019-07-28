12bde294aee6244bba690a9c7e301e8c5f0b991e
JWileczek <john.wileczek@prominentedge.com>
2019-07-28 17:42:37 -0400

    Expand original ContextManager service to allow more robust usage.

--- src/app/services/ContextManager/ContextManager.service.ts
+++ src/app/services/ContextManager/ContextManager.service.ts
@@ -6,7 +6,7 @@ import {Context, ContextState} from './Context';
   providedIn: 'root'
 })
 export class ContextManagerService {
-  private contexts = {};
+  private contexts: { [propName: string]: BehaviorSubject<Context> } = {};
   private defaultContext: BehaviorSubject<Context> = new BehaviorSubject(null);
 
   constructor() {
@@ -15,35 +15,88 @@ export class ContextManagerService {
   public registerContexts(contexts: Context | Context[]): void {
     if (Array.isArray(contexts)) {
       for (const context of contexts) {
-        this.contexts[context.uniqueId] = context;
+        this.contexts[context.uniqueId] = new BehaviorSubject(context);
       }
     } else {
-      this.contexts[contexts.uniqueId] = contexts;
+      this.contexts[contexts.uniqueId] = new BehaviorSubject(contexts);
     }
   }
 
-  public setDefaultContext(contextId: string) {
+  public setDefaultContext(contextId: string): void {
     const context = this.contexts[contextId];
     if (context == null) {
-      throw Error(`Context with Id: ${contextId} does not exist to set as default.`);
+      throw TypeError(`Context with Id: ${contextId} does not exist to set as default.`);
     }
-    this.defaultContext.next(context);
+    const contextObj = context.getValue();
+    this.defaultContext.next(contextObj);
   }
 
-  public setState(contextId: string, state: ContextState) {
-    const context = this.contexts[contextId];
-    context.state = state;
-    if (this.defaultContext.getValue() && this.defaultContext.getValue().uniqueId === contextId) {
-      this.defaultContext.next(context);
+  public setState(contextId: string, state: ContextState): void {
+    const contextSub = this.contexts[contextId];
+    const contextObj = contextSub.getValue();
+    contextObj.state = state;
+
+    this.updateContext(contextId, contextObj);
+  }
+
+  public updateContext(contextId: string, newContext: Context): void {
+    if (contextId !== newContext.uniqueId) {
+      throw TypeError('Can only update contexts with matching ids');
+    }
+
+    const contextSub = this.contexts[contextId];
+    if (contextSub == null) {
+      throw TypeError(`Context with Id: ${contextId} does not exist to update. Please register this context.`);
+    }
+    contextSub.next(newContext);
+
+    // Update default context object if this is also the default context.
+    const defaultContext = this.defaultContext.getValue();
+    if (defaultContext != null && defaultContext.uniqueId === contextId) {
+      this.defaultContext.next(newContext);
     }
   }
 
-  public getDefaultContext(): Observable<Context> {
+  public getDefaultContext(): Context {
+    return this.defaultContext.getValue();
+  }
+
+  public getDefaultObservable(): Observable<Context> {
     return this.defaultContext.asObservable();
   }
 
   public getContexts(): Context[] {
-    return Object.values(this.contexts);
+    const contextSubs = Object.values(this.contexts);
+    return contextSubs.map((sub) => sub.getValue());
+  }
+
+  public getContextObservables(): Observable<Context>[] {
+    const contextSubs = Object.values(this.contexts);
+    return contextSubs.map((sub) => sub.asObservable());
+  }
+
+  public getContext(contextId: string): Context {
+    return this.contexts[contextId].getValue();
+  }
+
+  public getContextObservable(contextId: string): Observable<Context> {
+    return this.contexts[contextId].asObservable();
+  }
+
+  public removeContext(contextId: string): void {
+    if (this.contexts[contextId] != null) {
+      // Complete will signal this observable shouldn't produce anymore updates.
+      this.contexts[contextId].complete();
+      delete this.contexts[contextId];
+    }
+  }
+
+  public clearContexts(): void {
+    // Gracefully complete each context before clearing.
+    const contextSubs = Object.values(this.contexts);
+    contextSubs.forEach((contextSub) => contextSub.complete());
+
+    this.contexts = {};
   }
 
 }
--- src/app/services/ContextManager/ContextManager.spec.ts
+++ src/app/services/ContextManager/ContextManager.spec.ts
@@ -4,32 +4,38 @@ import {Context, ContextState} from './Context';
 
 describe('ContextManagerService', () => {
   let contextManagerService;
+  let initialContexts;
 
-  it(`should provide full list of registered contexts`, async(() => {
+  beforeEach(() => {
     contextManagerService = new ContextManagerService();
-    expect(contextManagerService.getContexts().length).toEqual(0);
-    const initalContexts: Array<Context> = [{
+    initialContexts = [{
       uniqueId: 'First Context',
       state: ContextState.Online
     }, {
       uniqueId: 'Second Context',
       state: ContextState.Initializing
     }];
-    contextManagerService.registerContexts(initalContexts);
+    contextManagerService.registerContexts(initialContexts);
+  });
+
+  it(`Should have a null default context at initialization.`, async(() => {
+    expect(contextManagerService.getDefaultContext()).toEqual(null);
+  }));
+
+  it(`Should provide full list of registered contexts`, async(() => {
     expect(contextManagerService.getContexts().length).toEqual(2);
     const laterContext = {
-      uniqueId: 'ThirdContext',
+      uniqueId: 'Third Context',
       state: ContextState.Offline
     } as Context;
     contextManagerService.registerContexts(laterContext);
     expect(contextManagerService.getContexts().length).toEqual(3);
   }));
 
-  it(`should allow default context to be set`, async(() => {
-    contextManagerService = new ContextManagerService();
+  it(`Should allow default context to be set`, async(() => {
     let dContext;
     let expectedContext = null;
-    contextManagerService.getDefaultContext().subscribe((defaultContext) => {
+    contextManagerService.getDefaultObservable().subscribe((defaultContext) => {
       dContext = defaultContext;
       expect(dContext).toEqual(expectedContext);
     });
@@ -41,62 +47,84 @@ describe('ContextManagerService', () => {
     contextManagerService.setDefaultContext(expectedContext.uniqueId);
   }));
 
-  it(`contexts in list should update after state change`, async(() => {
-    contextManagerService = new ContextManagerService();
-    const initalContexts: Array<Context> = [{
-      uniqueId: 'First Context',
-      state: ContextState.Online
-    }, {
-      uniqueId: 'Second Context',
-      state: ContextState.Initializing
-    }];
-    contextManagerService.registerContexts(initalContexts);
-    contextManagerService.setState(initalContexts[1].uniqueId, ContextState.Online);
+  it(`Contexts in list should update after state change`, async(() => {
+    contextManagerService.setState(initialContexts[1].uniqueId, ContextState.Online);
     // Check that state was actually updated.
     expect(contextManagerService.getContexts()[1].state).toEqual(ContextState.Online);
   }));
 
   it(`Ensure update after state change of default.`, async(() => {
-    contextManagerService = new ContextManagerService();
     let dContext;
     let expectedContext = null;
-    contextManagerService.getDefaultContext().subscribe((defaultContext) => {
+    contextManagerService.getDefaultObservable().subscribe((defaultContext) => {
       dContext = defaultContext;
       expect(dContext).toEqual(expectedContext);
     });
-    const initalContexts: Array<Context> = [{
-      uniqueId: 'First Context',
-      state: ContextState.Online
-    }, {
-      uniqueId: 'Second Context',
-      state: ContextState.Initializing
-    }];
-    expectedContext = initalContexts[1];
-    contextManagerService.registerContexts(initalContexts);
+    expectedContext = initialContexts[1];
+    contextManagerService.registerContexts(initialContexts);
     contextManagerService.setDefaultContext(expectedContext.uniqueId);
     contextManagerService.setState(expectedContext.uniqueId, ContextState.Offline);
   }));
 
-  it(`Ensure no update after non-default state change.`, async(() => {
-    contextManagerService = new ContextManagerService();
+  it(`Ensure no update to default after non-default context state change.`, async(() => {
     let dContext = null;
     let expectedContext = null;
-    contextManagerService.getDefaultContext().subscribe((defaultContext) => {
+    contextManagerService.getDefaultObservable().subscribe((defaultContext) => {
       dContext = defaultContext;
       expect(dContext).toEqual(expectedContext);
     });
-    const initalContexts: Array<Context> = [{
-      uniqueId: 'First Context',
-      state: ContextState.Online
-    }, {
-      uniqueId: 'Second Context',
-      state: ContextState.Initializing
-    }];
-    expectedContext = initalContexts[1];
-    contextManagerService.registerContexts(initalContexts);
+    expectedContext = initialContexts[1];
+    contextManagerService.registerContexts(initialContexts);
     contextManagerService.setDefaultContext(expectedContext.uniqueId);
-    contextManagerService.setState(initalContexts[0].uniqueId, ContextState.Offline);
+    contextManagerService.setState(initialContexts[0].uniqueId, ContextState.Offline);
     expect(dContext).toEqual(expectedContext);
     expect(contextManagerService.getContexts()[0].state).toEqual(ContextState.Offline);
+  }));
+
+  it(`Should be able to remove context with valid Id`, async(() => {
+    expect(contextManagerService.getContexts().length).toEqual(2);
+
+    contextManagerService.removeContext(initialContexts[0].uniqueId);
+    expect(contextManagerService.getContexts().length).toEqual(1);
+  }));
+
+  it(`Should update context with valid Id, and notify subscribers`, async(() => {
+    expect(contextManagerService.getContexts().length).toEqual(2);
+    const laterContext = {
+      uniqueId: 'Third Context',
+      state: ContextState.Offline
+    } as Context;
+    const expectedContext = laterContext;
+    let expectedUpdateCount = 0;
+    let realUpdateCount = 0;
+    contextManagerService.registerContexts(laterContext);
+    contextManagerService.getContextObservable(laterContext.uniqueId).subscribe((context) => {
+      expect(context).toEqual(expectedContext);
+      expect(realUpdateCount++).toEqual(expectedUpdateCount);
+    });
+
+    laterContext.extraData = 'More Data';
+    expectedUpdateCount += 1;
+    contextManagerService.updateContext(laterContext.uniqueId, laterContext);
+  }));
+
+  it(`Should error when updating invalid context.`, async(() => {
+    expect(contextManagerService.getContexts().length).toEqual(2);
+    const laterContext = {
+      uniqueId: 'Third Context',
+      state: ContextState.Offline
+    } as Context;
+    expect(() => {
+      contextManagerService.updateContext(laterContext.uniqueId, initialContexts[0]);
+    }).toThrowError(TypeError);
+    expect(() => {
+      contextManagerService.updateContext(laterContext.uniqueId, laterContext);
+    }).toThrowError(TypeError);
+  }));
+
+  it(`Should clear contexts.`, async(() => {
+    expect(contextManagerService.getContexts().length).toEqual(2);
+    contextManagerService.clearContexts();
+    expect(contextManagerService.getContexts().length).toEqual(0);
   }));
 });
